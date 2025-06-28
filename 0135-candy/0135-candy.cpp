c02729e6ec6f6d0e7fb4df18129c5c3ded94401e
hardikbatra07 <hardikbatra345@gmail.com>
2025-06-28 12:54:35 +0530

    Time: 0 ms (100%), Space: 22.1 MB (85.87%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -1,43 +1,30 @@
 class Solution {
-    void dfs(int node, vector<vector<int>>& adj, vector<int>& a, vector<int>& candy, vector<int>& vis) {
-        if (vis[node] != -1) return;
-        
-        vis[node] = 0; 
-        for (auto it : adj[node]) {
-            if (vis[it] == -1) {
-                dfs(it, adj, a, candy, vis);
-            }
-            
-            if (a[node] > a[it]) {
-                candy[node] = max(candy[node], candy[it] + 1);
-            } else if (a[node] < a[it]) {
-                candy[it] = max(candy[it], candy[node] + 1);
-            }
-        }
-    }
 
 public:
     int candy(vector<int>& a) {
         int n = a.size();
-        vector<vector<int>> adj(n); 
-        for (int i = 0; i < n; ++i) {
-            if (i > 0) adj[i].push_back(i - 1);  
-            if (i < n - 1) adj[i].push_back(i + 1); 
-        }
-        
-        vector<int> candy(n, 1);  
-        vector<int> vis(n, -1);  
-        for (int i = 0; i < n; ++i) {
-            if (vis[i] == -1) { 
-                dfs(i, adj, a, candy, vis);
+        int i=1;
+        int sum=1;
+        while(i<n){
+            if(a[i]==a[i-1]){
+                sum++; i++;
+                continue;
             }
+            int peak=1;
+            while(i<n && a[i]>a[i-1]){
+                peak++;
+                sum+=peak;
+                i++;
+            }
+            int down=1;
+            while(i<n && a[i]<a[i-1]){
+                sum+=down;
+                down++;
+
+                i++;
+            }
+            if(down>peak) sum+=(down-peak);
         }
-        
-        int totalCandies = 0;
-        for (int i = 0; i < n; ++i) {
-            totalCandies += candy[i];
-        }
-        
-        return totalCandies;
+        return sum;
     }
 };
